919c60a946dc840a697cdaab59c6a35cf6f04840
hyeon ji <inthefield1027@gmail.com>
2021-02-26 00:13:04 +0900

    add login component

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,9 +1,11 @@
 import firebase from "firebase";
+import firebaseApp from "./firebase";
+
 class AuthService {
     //github로 들어올수도있고 구글로 들어올 수 있기때문에 이렇게 proviederName 설정해주기
     login(providerName) {
         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-        return firebase.auth().signInWithPopup(authProvider);
+        return firebaseApp.auth().signInWithPopup(authProvider);
     }
 }
 
--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,9 +1,13 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_KEY,
-    projectId: process.env.REACT_APP_FIREBASE_DB_URL,
     databaseURL: process.env.FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp;
