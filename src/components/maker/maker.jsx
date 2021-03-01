66cc041c3729522a822d02e80fa86eb8e4343419
hyeon ji <inthefield1027@gmail.com>
2021-03-01 15:51:55 +0900

    add CardAddForm component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -59,11 +59,17 @@ const Maker = ({ authService }) => {
             }
         });
     });
+
+    const addCard = (card) => {
+        const updated = [...cards, card];
+        setCards(updated);
+    };
+
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout} className={styles.header} />
             <div className={styles.container}>
-                <Editor cards={cards} />
+                <Editor cards={cards} addCard={addCard} />
                 <Preview cards={cards} />
             </div>
             <Footer className={styles.footer} />
