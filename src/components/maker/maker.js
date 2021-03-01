ef171fc08eaa27ec275638ce74ab0c50974118fb
hyeon ji <inthefield1027@gmail.com>
2021-03-01 13:53:26 +0900

    add card and edit form-component

--- src/components/maker/maker.js
+++ src/components/maker/maker.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./maker.module.css";
 import Footer from "../footer/footer";
 import Header from "../header/header";
@@ -6,10 +6,47 @@ import { useHistory } from "react-router-dom";
 import Editor from "../editor/editor";
 import Preview from "../preview/preview";
 
-//Maker에서 suthService를통해서 로그아웃 시켜준다
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        //수동적 데이터 미리 만들어보기
+        {
+            id: "1",
+            name: "hyeon1",
+            company: "Ola",
+            theme: "dark",
+            title: "software Engineer",
+            email: "inthefield1027@gmail.com",
+            message: "go for it",
+            fileName: "hj",
+            fileURL: null,
+        },
+        {
+            id: "2",
+            name: "hyeon2",
+            company: "Ola",
+            theme: "colorful",
+            title: "singer",
+            email: "inthefield1027@gmail.com",
+            message: "go for it",
+            fileName: "hj",
+            fileURL: "hj.png",
+        },
+        {
+            id: "3",
+            name: "hyeon3",
+            company: "Ola",
+            theme: "light",
+            title: "writer",
+            email: "inthefield1027@gmail.com",
+            message: "go for it",
+            fileName: "hj",
+            fileURL: null,
+        },
+    ]);
     const history = useHistory();
+
     //logout 로직
+    //Maker에서 suthService를통해서 로그아웃 시켜준다
     const onLogout = () => {
         authService.logout();
     };
@@ -17,6 +54,7 @@ const Maker = ({ authService }) => {
     useEffect(() => {
         authService.onAuthChange((user) => {
             if (!user) {
+                //유저가 없다면 다시 홈화면 렌더
                 history.push("/");
             }
         });
@@ -25,8 +63,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} className={styles.header} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer className={styles.footer} />
         </section>
