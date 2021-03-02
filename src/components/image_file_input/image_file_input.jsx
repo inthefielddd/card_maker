82b2f9ce0796226051f5c8d0093d894cefa745cc
hyeon ji <inthefield1027@gmail.com>
2021-03-02 18:10:22 +0900

    add loading spinner for file input

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,16 +1,19 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({imageUploader, name, onFileChange}) => {
+    const [loading, setLoading] = useState(false);
     const inputRef = useRef();
     const onButtonClick = (event) =>{
         event.preventDefault();
         inputRef.current.click();
     }
 
+    //파일이 url로 변경 될때
     const onChange = async event =>{
-        console.log(event.target.files[0]);
-        const uploaded = await imageUploader.upload(event.target.files[0]);
+        setLoading(true);
+        const uploaded = await imageUploader.upload(event.target.files[0]); //파일다 받아왔을떄
+        setLoading(false);
         console.log(uploaded);
         //파일 올리면 값을 받아온다.
         //onFileChange를 화면에서 보일 수 있게 하기 위해서 전달해줘야한다.
@@ -29,9 +32,18 @@ const ImageFileInput = ({imageUploader, name, onFileChange}) => {
             accept="image/*" 
             name="file"
             onChange={onChange} />
-            <button className={styles.button} onClick={onButtonClick}>
-                {name || 'No File'}
-            </button>
+            {!loading && (
+             <button className={`${styles.button} ${name ? styles.pink: styles.grey}`} onClick={onButtonClick}>
+                 {name || 'No File'}
+             </button>
+             
+            )}
+           
+            {
+            //로딩중일떄
+            loading &&  <div className={styles.loading}></div>
+            }
+          
         </div>
  
     )
